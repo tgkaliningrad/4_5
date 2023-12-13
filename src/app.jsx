beb5ce52206566c3ddb59587e6d7c292687ef761
tgkaliningrad <119241391+tgkaliningrad@users.noreply.github.com>
2023-12-13 09:14:15 +0200

    001

---
+++ src/app.jsx
@@ -0,0 +1,19 @@
+
+
+import React from 'react';
+
+function App() {
+    return (
+        <h1 className="title">Привет, мир!!! Создавать React приложения с CRA очень легко и быстро</h1>
+    );
+}
+
+export default App;
+
+
+
+
+
+
+
+
